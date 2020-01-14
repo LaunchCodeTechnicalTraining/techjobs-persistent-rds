4f99405f14b3715c497ee41226ecfd2331e243e2
Carly Langlois <cj.langlois.cl@gmail.com>
2020-01-14 11:11:19 -0600

    reintroduce list and search functionality

---
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
@@ -0,0 +1,72 @@
+package org.launchcode.javawebdevtechjobspersistent.controllers;
+
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.launchcode.javawebdevtechjobspersistent.models.Job;
+import org.launchcode.javawebdevtechjobspersistent.models.Skill;
+import org.launchcode.javawebdevtechjobspersistent.models.data.EmployerRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.JobRepository;
+import org.launchcode.javawebdevtechjobspersistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.launchcode.javawebdevtechjobspersistent.models.JobData;
+
+import java.util.HashMap;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping(value = "list")
+public class ListController {
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private EmployerRepository employerRepository;
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    static HashMap<String, String> columnChoices = new HashMap<>();
+    static HashMap<String, Object> tableChoices = new HashMap<>();
+
+    public ListController (JobRepository jobRepository, EmployerRepository employerRepository, SkillRepository skillRepository) {
+        columnChoices.put("all", "All");
+        columnChoices.put("employer", "Employer");
+        columnChoices.put("skills", "Skills");
+        //tableChoices.put("all", "View All");
+        tableChoices.put("employer", employerRepository.findAll());
+        tableChoices.put("skills", skillRepository.findAll());
+
+    }
+
+    @RequestMapping("")
+    public String list(Model model) {
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
+        model.addAttribute("tableChoices", tableChoices);
+
+        return "list";
+    }
+
+    @RequestMapping(value = "jobs")
+    public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam String value) {
+        Iterable<Job> jobs;
+        if (column.toLowerCase().equals("all")){
+            jobs = jobRepository.findAll();
+            model.addAttribute("title", "All Jobs");
+        } else {
+            jobs = JobData.findByColumnAndValue(column, value, jobRepository.findAll());
+            model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+        }
+        model.addAttribute("jobs", jobs);
+
+        return "list-jobs";
+    }
+}
